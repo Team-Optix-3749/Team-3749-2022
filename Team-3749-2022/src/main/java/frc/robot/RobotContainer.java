0eea14499fa6461c178567ee0ad90e24ff8a49f7
SAnd353 <steven92127@gmail.com>
2022-01-11 17:20:38 -0800

    Finished pathweaver stuff
    Co-authored-by: Toby-Leeder <Toby-Leeder@users.noreply.github.com>

--- Team-3749-2022/src/main/java/frc/robot/RobotContainer.java
+++ Team-3749-2022/src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,29 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 
 /**
@@ -48,7 +66,52 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+
+    // Create a voltage constraint to ensure we don't accelerate too fast
+    var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(Constants.Drivetrain.ksVolts, Constants.Drivetrain.kvVoltSecondsPerMeter,Constants.Drivetrain.kaVoltSecondsSquaredPerMeter), Constants.Drivetrain.kDriveKinematics, 10);
+
+    // Create config for trajectory
+    TrajectoryConfig config =
+        new TrajectoryConfig(Constants.Drivetrain.kMaxSpeedMetersPerSecond, Constants.Drivetrain.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+          .setKinematics(Constants.Drivetrain.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint);
+
+    // An example trajectory to follow.  All units in meters.
+
+    Trajectory exampleTrajectory = new Trajectory();
+
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("paths/test.json");
+      exampleTrajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+   } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory", ex.getStackTrace());
+   }
+
+    RamseteCommand ramseteCommand =
+        new RamseteCommand(
+            exampleTrajectory,
+            m_drivetrain::getPose,
+            new RamseteController(Constants.Drivetrain.kRamseteB, Constants.Drivetrain.kRamseteZeta),
+            new SimpleMotorFeedforward(
+                Constants.Drivetrain.ksVolts,
+                Constants.Drivetrain.kvVoltSecondsPerMeter,
+                Constants.Drivetrain.kaVoltSecondsSquaredPerMeter),
+            Constants.Drivetrain.kDriveKinematics,
+            m_drivetrain::getWheelSpeeds,
+            new PIDController(Constants.Drivetrain.kPDriveVel, 0, 0),
+            new PIDController(Constants.Drivetrain.kPDriveVel, 0, 0),
+            // RamseteCommand passes volts to the callback
+            m_drivetrain::tankDriveVolts,
+            m_drivetrain);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return ramseteCommand.andThen(() -> m_drivetrain.tankDriveVolts(0, 0));
   }
 }

7b42f4fcb140e9b3ec2b9c20485e571679c3a38f
Dash Penning <dashpenning@gmail.com>
2022-01-25 17:29:55 -0800

    simple shooter

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Shooter.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.MotorCommutation;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
@@ -13,6 +15,8 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -21,70 +25,25 @@ import frc.robot.Constants;
  * @author Rohin Sood
  */
 public class Shooter extends SubsystemBase{
-    private CANSparkMax m_shooterMotor;
-    private CANPIDController m_controller;
-    private CANEncoder m_encorder;
-    
+    private WPI_TalonFX m_motorLeft = new WPI_TalonFX(Constants.Shooter.motorLeft);
+    private WPI_TalonFX m_motorRight = new WPI_TalonFX(Constants.Shooter.motorRight);
+    private MotorControllerGroup m_shooterMotor = new MotorControllerGroup(m_motorLeft, m_motorRight);
 
     private PIDController m_pidController;
-    
-    private WPI_TalonSRX m_belt_front = new WPI_TalonSRX(Constants.Shooter.belt_front);
-    private VictorSPX m_belt_back = new VictorSPX(Constants.Shooter.belt_back);
-
     public Shooter(){
-        //Get shooter motor
-        m_shooterMotor = new CANSparkMax(Constants.Shooter.motor, MotorType.kBrushless);
-        //Don't brake it if you don't need to
-        m_shooterMotor.setIdleMode(IdleMode.kCoast);
-        //Shoots out of the front, not the intake
-        m_shooterMotor.setInverted(true);
-        //Get shooter encoder
-        m_encorder = m_shooterMotor.getEncoder();
-        //Link encoder to controller
-        m_controller = m_shooterMotor.getPIDController();
-        m_controller.setFeedbackDevice(m_encorder);
-        m_controller.setP(Constants.Shooter.kP);
-        m_controller.setI(Constants.Shooter.kI);
-        m_controller.setD(Constants.Shooter.kD);
-        //reset the motor
-        if (resetShooter()){
-            System.out.println("Motor cannot reset. May prevent PID from functioning properlly.");
-        }
-        //set the PID values
-        m_pidController = new PIDController(Constants.Shooter.kP, Constants.Shooter.kI, Constants.Shooter.kD);
-        //reverse the belt so that it intakes
-        m_belt_back.setInverted(true);
+        m_motorRight.setInverted(true);
     }
 
-    public boolean resetShooter(){
-        m_controller.setReference(0, ControlType.kDutyCycle);
-        m_shooterMotor.stopMotor();
-        return m_encorder.getVelocity() > 0.1;
+    public void stopMotor(){
+        m_shooterMotor.set(0);
     }
-    public double getVelocity(){
-        return m_encorder.getVelocity();
+    public double getLeftVelocity(){
+        return m_motorLeft.getSelectedSensorVelocity();
     }
-    public boolean setShooter(double current, double target){
+    public double getRightVelocity(){
+        return m_motorRight.getSelectedSensorVelocity();
+    }
+    public void setShooter(double current, double target){
         m_shooterMotor.set(m_pidController.calculate(current, target) * 0.004);
-        System.out.println(m_pidController.calculate(current, target) * 0.004);
-        if (m_pidController.calculate(current, target) * 0.004 == 0){
-            return true;
-        }
-        return false;
-    }
-    public void beltUp() {
-        m_belt_front.set(ControlMode.PercentOutput, -Constants.Shooter.beltSpeed);
-        m_belt_back.set(ControlMode.PercentOutput, -Constants.Shooter.beltSpeed);
-    }
-    public void beltDown() {
-        m_belt_front.set(ControlMode.PercentOutput, Constants.Shooter.beltSpeed);
-        m_belt_back.set(ControlMode.PercentOutput, Constants.Shooter.beltSpeed);
-    }
-    public void beltStop() {
-        m_belt_front.set(ControlMode.PercentOutput,0.0);
-        m_belt_back.set(ControlMode.PercentOutput,0.0);
-    }
-    public void shoot(){
-        m_controller.setReference(Constants.Shooter.maxVoltage, ControlType.kDutyCycle);
     }
 }

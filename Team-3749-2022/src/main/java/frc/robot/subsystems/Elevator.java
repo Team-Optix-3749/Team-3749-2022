15fbd2efec12c86cda926944748f97c2eec97e1a
Rohin Sood <rs.rohinsood@gmail.com>
2022-01-26 16:27:51 -0800

    Co-authored-by: Ple1ades <Ple1ades@users.noreply.github.com> Co-authored-by: SlothInTheHat <SlothInTheHat@users.noreply.github.com>

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Elevator.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,21 +1,49 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.ctre.phoenix.sensors.WPI_CANCoder;
+
+import frc.robot.Constants;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
-  public Elevator() {}
+    
+    public CANSparkMax m_rightTilt;
+    public CANSparkMax m_leftTilt;
+    private final PIDController m_tiltPIDController = new PIDController(Constants.Auto.kP, Constants.Auto.kI, Constants.Auto.kD);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    private void setTiltMotors(double speed) {
+        m_rightTilt.set(speed);
+        m_leftTilt.set(speed);
+    }
+    
+    public CANSparkMax m_chain;
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    public Elevator() {
+        m_rightTilt = new CANSparkMax(Constants.Elevator.rightTilt, MotorType.kBrushless);
+        m_rightTilt.setIdleMode(IdleMode.kCoast);
+
+        m_leftTilt = new CANSparkMax(Constants.Intake.liftMotor, MotorType.kBrushless);
+        m_leftTilt.setIdleMode(IdleMode.kBrake);
+
+        m_chain = new CANSparkMax(Constants.Intake.intakeMotor, MotorType.kBrushless);
+        m_chain.setIdleMode(IdleMode.kBrake);
+    }
+
+    public void tiltForward() {
+        m_rightTilt.set(0);
+
+    }
+
+
+
+
 }

9665810f598f0b6e0646645b80147f752a7e48ef
Nikhil Lalwani <lalwaninikhil42@gmail.com>
2022-02-01 16:59:24 -0800

    ok now its done

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,18 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+
+
+
+/**
+ * Intake Subsystem
+ * 
+ * @author Nikhil Lalwani
+ * @author Junnie Niu
+ * @author Aniket Chakradeo
+ * 
+ */
+
 package frc.robot.subsystems;
  
 import com.ctre.phoenix.sensors.WPI_CANCoder;

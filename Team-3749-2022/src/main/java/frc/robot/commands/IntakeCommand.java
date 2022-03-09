b10c92525245924ae4dc5d6f9db458268aeefeaa
Raadwan Masum <piraadwan@gmail.com>
2022-03-09 08:56:34 -0800

    full shintake routine with shintakehold and extend

--- Team-3749-2022/src/main/java/frc/robot/commands/IntakeCommand.java
+++ Team-3749-2022/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -4,7 +4,8 @@
  
 package frc.robot.commands;
  
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.*;
+import frc.robot.utilities.Constants;
 import frc.robot.utilities.Controls;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -14,6 +15,7 @@ import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 public class IntakeCommand extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     private final Intake m_intake;
+    private final Shintake m_shintake;
 
     private boolean comp = false;
 
@@ -23,10 +25,12 @@ public class IntakeCommand extends CommandBase {
      * @param intake The subsystem used by this command.
      * @param solenoid The subsystem used for solenoid
      */
-    public IntakeCommand(Intake intake) {
+    public IntakeCommand(Intake intake, Shintake shintake) {
         m_intake = intake;
+        m_shintake = shintake;
+
         // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(intake);
+        addRequirements(intake, shintake);
     }
 
     // Called when the command is initially scheduled.
@@ -39,23 +43,26 @@ public class IntakeCommand extends CommandBase {
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {        
-        if(Controls.Intake.intakeBtn.getAsBoolean()) {
+    public void execute() {
+        if (Constants.round(Controls.Intake.intakeBtn.getAsDouble()) > 0) {
+            // m_intake.intakePneumatics(kForward);
             m_intake.setIntake(1);
-            m_intake.intakePneumatics(kForward);
+            m_shintake.holdShintake();
+            System.out.println("asdf");
         } 
-        if (Controls.aTestBtn.getAsBoolean()) m_intake.setIntake(1); 
+        else if (Controls.aTestBtn.getAsBoolean()) m_intake.setIntake(1); 
         else {
             m_intake.stopMotors();
-            m_intake.intakePneumatics(kReverse);
+            m_shintake.stopMotors();
+            System.out.println("asdf1");
+            // m_intake.intakePneumatics(kReverse);
         }
 
-        if (Controls.Intake.compBtn.getAsBoolean()) {
-            if (comp) { m_intake.startCompressor(); comp = false; }
-            else if (comp == false) { m_intake.stopCompressor(); comp = true; }
-        } 
+        if (Controls.Intake.compBtn.getAsBoolean()) { m_intake.startCompressor();             System.out.println("asdf2");}
+        else if (comp == false) { m_intake.stopCompressor(); }
+         
 
-        if(Controls.Intake.pistonBtn.getAsBoolean()) m_intake.intakePneumatics(kForward);
+        if(Constants.round(Controls.Intake.intakeBtn.getAsDouble()) > 0) m_intake.intakePneumatics(kForward);
         else m_intake.intakePneumatics(kReverse);
     }
 

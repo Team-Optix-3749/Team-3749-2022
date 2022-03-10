95589431b00e5f9e228f15410f079ebb973979f4
Raadwan Masum <piraadwan@gmail.com>
2022-03-09 21:36:41 -0800

    set intake to brake mode

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.Constants;
@@ -24,6 +25,10 @@ public class Intake extends SubsystemBase {
 
     public Intake() {
         m_shintakeBack.setInverted(true);
+
+        m_intakeMotor.setIdleMode(IdleMode.kBrake);
+        m_shintakeFront.setIdleMode(IdleMode.kBrake);
+        m_shintakeBack.setIdleMode(IdleMode.kBrake);
     }
 
     public void setIntake(int dir) {

b5a579b6c3636ad83e8a78bcd9c749ce7e064c80
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 08:15:51 -0800

    fix shooter to use boolean supplier

--- Team-3749-2022/src/main/java/frc/robot/commands/shooter/Shoot.java
+++ Team-3749-2022/src/main/java/frc/robot/commands/shooter/Shoot.java
@@ -2,8 +2,12 @@ package frc.robot.commands.shooter;
 
 import frc.robot.subsystems.*;
 import frc.robot.utilities.Constants;
+import frc.robot.utilities.Xbox.*;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+
+import com.fasterxml.jackson.databind.ser.std.BooleanSerializer;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -12,9 +16,9 @@ public class Shoot extends CommandBase {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
     private final Shooter m_shooter;
-    private final DoubleSupplier m_trigger;
+    private final BooleanSupplier m_trigger;
 
-    public Shoot(Shooter shooter, DoubleSupplier trigger) {
+    public Shoot(Shooter shooter, BooleanSupplier trigger) {
         m_shooter = shooter;
         m_trigger = trigger;
         addRequirements(shooter);
@@ -27,10 +31,12 @@ public class Shoot extends CommandBase {
 
     @Override
     public void execute() {
-        boolean shooterBtn = Constants.round(m_trigger.getAsDouble()) > 0;
-
-        if (shooterBtn)
+        if (m_trigger.getAsBoolean())
             m_shooter.setVelocity(1000);
+        else
+            m_shooter.stopMotor();
+
+
     }
 
     @Override

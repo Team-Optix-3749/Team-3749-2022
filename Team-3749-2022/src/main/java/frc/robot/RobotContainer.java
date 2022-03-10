6e1fc48dd5a362730c39d15167c2890072e4ea57
Raadwan Masum <piraadwan@gmail.com>
2022-03-09 23:17:28 -0800

    configure button bindings

--- Team-3749-2022/src/main/java/frc/robot/RobotContainer.java
+++ Team-3749-2022/src/main/java/frc/robot/RobotContainer.java
@@ -14,15 +14,15 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.commands.intake.*;
+import frc.robot.commands.elevator.*;
 import frc.robot.commands.shooter.*;
 import frc.robot.subsystems.*;
 import frc.robot.utilities.Constants;
-import frc.robot.utilities.Xbox.*;
+import frc.robot.utilities.Xbox;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -43,34 +43,43 @@ public class RobotContainer {
 
     private final Elevator m_elevator = new Elevator();
 
+    Xbox Pilot, Operator;
+
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
         // Configure the button bindings
         configureButtonBindings();
-
-        m_drivetrain.setDefaultCommand(
-                new ArcadeDrive(m_drivetrain, Pilot.leftJoystickY, Pilot.rightJoystickX));
-        
-        m_shooter.setDefaultCommand(new RawShoot(m_shooter, Pilot.rt));
-
-        m_intake.setDefaultCommand(new IntakeHold(m_intake, Pilot.lt));
-
-        // m_elevator.setDefaultCommand(new Extend(m_intake, Pilot.lt));
     }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
     private void configureButtonBindings() {
-        // Pilot.rb.whenHeld(new IntakeHold(m_intake));
-        Pilot.x.whenHeld(new Compressor(m_intake));
+        Pilot = new Xbox(0);
+        Operator = new Xbox(1);
+
+        Pilot.x().whenPressed(new InstantCommand(m_intake::startCompressor))
+            .whenReleased(new InstantCommand(m_intake::stopCompressor));
+
+        Pilot.povUp().whenPressed(new Extend(m_elevator))
+            .whenReleased(new InstantCommand(m_elevator::stopClimb));
+
+        Pilot.povDown().whenPressed(new Lift(m_elevator))
+            .whenReleased(new InstantCommand(m_elevator::stopClimb));
+
+        Pilot.povLeft().whenPressed(new Untilt(m_elevator))
+            .whenReleased(new InstantCommand(m_elevator::stopTilt));
+
+        Pilot.povRight().whenPressed(new Tilt(m_elevator))
+            .whenReleased(new InstantCommand(m_elevator::stopTilt));
+
+        m_drivetrain.setDefaultCommand(
+            new ArcadeDrive(m_drivetrain, Pilot::getLeftY, Pilot::getRightX));
+
+        m_shooter.setDefaultCommand(
+            new RawShoot(m_shooter, Pilot::getRightTrigger));
+
+        m_intake.setDefaultCommand(
+            new IntakeHold(m_intake, Pilot::getLeftTrigger));
     }
 
     /**

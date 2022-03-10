7dfb9ddaf1422ea35b3e49235dea966c153126cd
Raadwan Masum <piraadwan@gmail.com>
2022-03-09 20:10:47 -0800

    create shooter subsystem

---
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.Constants;
+
+public class Shooter extends SubsystemBase{
+    private WPI_TalonFX m_leftShooterMotor = new WPI_TalonFX(Constants.Shooter.leftShooterMotor);
+    private WPI_TalonFX m_rightShooterMotor = new WPI_TalonFX(Constants.Shooter.rightShooterMotor);
+
+    private CANSparkMax m_turretMotor = new CANSparkMax(Constants.Shooter.turretMotor, MotorType.kBrushless);
+    private RelativeEncoder m_turretEncoder = m_turretMotor.getEncoder();
+
+    private MotorControllerGroup m_shooterMotors = new MotorControllerGroup(m_leftShooterMotor, m_rightShooterMotor);
+
+    private PIDController m_pidController = new PIDController(Constants.Shooter.kP, Constants.Shooter.kI, Constants.Shooter.kD);
+
+    public Shooter() {
+        m_rightShooterMotor.setInverted(true);
+        m_turretEncoder.setPositionConversionFactor(Constants.Shooter.gearRatio);
+        m_leftShooterMotor.setNeutralMode(NeutralMode.Coast);
+        m_rightShooterMotor.setNeutralMode(NeutralMode.Coast);
+    }
+
+    public void setShooterRaw(double speed) {
+        m_shooterMotors.set(speed);
+    }
+
+    public void stopShooter() {
+        m_shooterMotors.set(0);
+    }
+
+    public void velocityShoot() {
+       
+    }
+}

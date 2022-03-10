1a2e264226b4c356ec6e044cd279152af7856e1c
Raadwan Masum <piraadwan@gmail.com>
2022-03-09 22:19:11 -0800

    fix compressor command
    Co-authored-by: Rohin Sood <rs.rohinsood@gmail.com>
    Co-authored-by: Toby-Leeder <Toby-Leeder@users.noreply.github.com>

--- Team-3749-2022/src/main/java/frc/robot/commands/intake/Compressor.java
+++ Team-3749-2022/src/main/java/frc/robot/commands/intake/Compressor.java
@@ -1,21 +1,15 @@
 package frc.robot.commands.intake;
 
 import frc.robot.subsystems.*;
-import frc.robot.utilities.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
-
-import java.util.function.DoubleSupplier;
 
 public class Compressor extends CommandBase {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
     
     private final Intake m_intake;
-    private final DoubleSupplier m_trigger;
 
-    public Compressor(Intake intake, DoubleSupplier trigger) {
+    public Compressor(Intake intake) {
         m_intake = intake;
-        m_trigger = trigger;
         addRequirements(intake);
     }
 

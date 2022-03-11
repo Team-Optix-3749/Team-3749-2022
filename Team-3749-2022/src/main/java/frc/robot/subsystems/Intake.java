105b75e72958a833064a8166b280296e70e317aa
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 20:44:30 -0800

    remove unused imports

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Intake.java
@@ -6,7 +6,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.Constants;
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 

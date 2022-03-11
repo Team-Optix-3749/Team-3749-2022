d1ab99a277c9a07fde2724ff41be6201eb4ec009
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 20:39:30 -0800

    create vision shoot command
    Co-authored-by: Rohin Sood <rs.rohinsood@gmail.com>

---
+++ Team-3749-2022/src/main/java/frc/robot/commands/shooter/VisionShoot.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.shooter;
+
+import frc.robot.subsystems.*;
+import frc.robot.utilities.Constants;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class VisionShoot extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+
+    private final Shooter m_shooter;
+    private final BooleanSupplier m_trigger;
+
+    public VisionShoot(Shooter shooter, BooleanSupplier trigger) {
+        m_shooter = shooter;
+        m_trigger = trigger;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void initialize() {
+        m_shooter.stopMotor();
+    }
+
+    @Override
+    public void execute() {
+        if (m_trigger.getAsBoolean())
+            m_shooter.setRPM(Constants.Shooter.shooterRPM);
+        else
+            m_shooter.stopMotor();
+            
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_shooter.stopMotor();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}

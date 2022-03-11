0c5f43894982c1345f98aee4bf062150fccbdae5
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 20:09:11 -0800

    implement rotate speed constant

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -42,6 +42,6 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public void arcadeDrive(double speed, double rotation) {
-        m_drive.arcadeDrive(speed, -rotation);
+        m_drive.arcadeDrive(speed, -rotation * Constants.Drivetrain.rotationalSpeed);
     }
 }

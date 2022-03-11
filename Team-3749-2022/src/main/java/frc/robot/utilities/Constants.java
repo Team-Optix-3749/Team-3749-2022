ecf089455e9f212a68ec154cc807276cdf7ee013
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 20:12:26 -0800

    create shooter rpm constant

--- Team-3749-2022/src/main/java/frc/robot/utilities/Constants.java
+++ Team-3749-2022/src/main/java/frc/robot/utilities/Constants.java
@@ -67,7 +67,7 @@ public final class Constants {
     }
 
     public static final class Shooter{
-        public static final double shooterSpeed = 1.0;
+        public static final double shooterRPM = 5000;
 
         public static final int turretMotor = 22;
         public static final int rightShooterMotor = 15;

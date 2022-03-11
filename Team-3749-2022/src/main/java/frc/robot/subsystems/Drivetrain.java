0be11a7b7e955b32bc036428055c77acff87f03b
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 22:43:33 -0800

    add advanced control to drivetrain

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -25,10 +24,9 @@ public class Drivetrain extends SubsystemBase {
 
     private DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
 
-    private final Encoder m_leftEncoder = new Encoder(0, 1);
-    private final Encoder m_rightEncoder = new Encoder(2, 3);
-
     private final Gyro m_gyro = new AHRS();
+
+    private final DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
 
     public Drivetrain() {
         m_leftFront.setNeutralMode(NeutralMode.Coast);
@@ -37,15 +35,115 @@ public class Drivetrain extends SubsystemBase {
         m_rightBack.setNeutralMode(NeutralMode.Coast);
         m_rightFront.setNeutralMode(NeutralMode.Coast);
 
-
         m_right.setInverted(true);
+        // resetEncoders();
     }
 
     public void arcadeDrive(double speed, double rotation) {
         m_drive.arcadeDrive(speed, -rotation * Constants.Drivetrain.rotationalSpeed);
     }
-    
-    public void visionAlignDrivetrain(double x){
-        arcadeDrive(0, x/29.8);
+
+    public void visionAlignDrivetrain(double x) {
+        arcadeDrive(0, x / 29.8);
+    }
+
+    @Override
+    public void periodic() {
+        // Update the odometry in the periodic block
+        m_odometry.update(
+                m_gyro.getRotation2d(), m_leftFront.getSelectedSensorPosition(),
+                m_rightFront.getSelectedSensorPosition());
+    }
+
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+    public Pose2d getPose() {
+        // System.out.println(m_odometry.getPoseMeters().getX() + " " +
+        // m_odometry.getPoseMeters().getY());
+        return m_odometry.getPoseMeters();
+    }
+
+    /**
+     * Returns the current wheel speeds of the robot.
+     *
+     * @return The current wheel speeds.
+     */
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(m_leftFront.getSelectedSensorVelocity(),
+                m_rightFront.getSelectedSensorVelocity());
+    }
+
+    /**
+     * Resets the odometry to the specified pose.
+     *
+     * @param pose The pose to which to set the odometry.
+     */
+    public void resetOdometry(Pose2d pose) {
+        // resetEncoders();
+        m_odometry.resetPosition(pose, m_gyro.getRotation2d());
+    }
+
+    /**
+     * Controls the left and right sides of the drive directly with voltages.
+     *
+     * @param leftVolts  the commanded left output
+     * @param rightVolts the commanded right output
+     */
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        m_left.setVoltage(leftVolts);
+        m_right.setVoltage(rightVolts);
+        m_drive.feed();
+    }
+
+    /** Resets the drive encoders to currently read a position of 0. */
+    // public void resetEncoders() {
+    // m_leftFront.
+    // m_leftEncoder.reset();
+    // m_rightEncoder.reset();
+    // }
+
+    /**
+     * Gets the average distance of the two encoders.
+     *
+     * @return the average of the two encoder readings
+     */
+    public double getAverageEncoderDistance() {
+        return (m_leftFront.getSelectedSensorPosition() + m_rightFront.getSelectedSensorPosition()) / 2.0;
+    }
+
+    /**
+     * Sets the max output of the drive. Useful for scaling the drive to drive more
+     * slowly.
+     *
+     * @param maxOutput the maximum output to which the drive will be constrained
+     */
+    public void setMaxOutput(double maxOutput) {
+        m_drive.setMaxOutput(maxOutput);
+    }
+
+    /** Zeroes the heading of the robot. */
+    public void zeroHeading() {
+        m_gyro.reset();
+    }
+
+    /**
+     * Returns the heading of the robot.
+     *
+     * @return the robot's heading in degrees, from -180 to 180
+     */
+    public double getHeading() {
+        return m_gyro.getRotation2d().getDegrees();
+    }
+
+    /**
+     * Returns the turn rate of the robot.
+     *
+     * @return The turn rate of the robot, in degrees per second
+     */
+    public double getTurnRate() {
+        return -m_gyro.getRate();
     }
 }

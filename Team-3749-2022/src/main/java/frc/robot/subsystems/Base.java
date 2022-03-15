6fdfa0befd7235d49065f78108b7257cd4349958
Rohin Sood <rs.rohinsood@gmail.com>
2022-03-14 22:16:38 -0700

    fixed balls.cope()

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Base.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Base.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Base extends SubsystemBase {
@@ -12,7 +13,11 @@ public class Base extends SubsystemBase {
 
 
 
-    public void cope() throws IOException {
-        Path trajpath = Filesystem.getDeployDirectory().toPath().resolve("cope lol");
+    public void cope() {    
+        int x = 0;
+        while (true) {
+            x++;
+            SmartDashboard.putString("cope " + x, "L ur mom lol cope harder");
+        }
     }
 }

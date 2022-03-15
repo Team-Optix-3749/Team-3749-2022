cd3972adf60036550a174cdaf81d1cc04aee8bdc
Raadwan Masum <piraadwan@gmail.com>
2022-03-14 21:45:22 -0700

    m_balls.cope()

--- Team-3749-2022/src/main/java/frc/robot/subsystems/Base.java
+++ Team-3749-2022/src/main/java/frc/robot/subsystems/Base.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Base extends SubsystemBase {
+    public Base(){}
 
+
+
+    public void cope() throws IOException {
+        Path trajpath = Filesystem.getDeployDirectory().toPath().resolve("cope lol");
+    }
 }
